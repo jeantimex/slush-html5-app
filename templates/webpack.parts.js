2c726a16fec88cfa8932fa5335fd0d4fa39749b4
jeantimex <jean.timex@gmail.com>
2019-09-26 22:49:20 -0700

    Support multiple pages.

---
+++ templates/webpack.parts.js
@@ -0,0 +1,144 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const PurgecssPlugin = require('purgecss-webpack-plugin');
+const UglifyWebpackPlugin = require('uglifyjs-webpack-plugin');
+const cssnano = require('cssnano');
+const webpack = require('webpack');
+
+exports.page = ({
+  name = '',
+  path = '',
+  template = require.resolve('html-webpack-plugin/default_index.ejs'),
+  title = '',
+  entry,
+  chunks,
+} = {}) => ({
+  entry,
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks,
+      filename: `${path && path + '/'}${name || 'index'}.html`,
+      template,
+      title,
+    }),
+  ],
+});
+
+exports.clean = path => ({
+  plugins: [new CleanWebpackPlugin()],
+});
+
+exports.purifyCSS = ({ paths }) => ({
+  plugins: [new PurgecssPlugin({ paths })],
+});
+
+exports.minifyCSS = ({ options }) => ({
+  plugins: [
+    new OptimizeCSSAssetsPlugin({
+      cssProcessor: cssnano,
+      cssProcessorOptions: options,
+      canPrint: false,
+    }),
+  ],
+});
+
+exports.extractCSS = ({ include, exclude, use = [] }) => {
+  // Output extracted CSS to a file
+  const plugin = new MiniCssExtractPlugin({
+    filename: '[name].[contenthash:4].css',
+  });
+
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.(scss|css)$/,
+          include,
+          exclude,
+          use: [MiniCssExtractPlugin.loader].concat(use),
+        },
+      ],
+    },
+    plugins: [plugin],
+  };
+};
+
+exports.autoprefix = () => ({
+  loader: 'postcss-loader',
+  options: {
+    plugins: () => [require('autoprefixer')()],
+  },
+});
+
+exports.minifyJavaScript = () => ({
+  optimization: {
+    minimizer: [new UglifyWebpackPlugin({ sourceMap: true })],
+  },
+});
+
+exports.devServer = ({ host, port } = {}) => ({
+  devServer: {
+    stats: 'errors-only',
+    host, // Defaults to `localhost`
+    port, // Defaults to 8080
+    open: true,
+    overlay: true,
+  },
+});
+
+exports.loadJavaScript = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include,
+        exclude,
+        use: 'babel-loader',
+      },
+    ],
+  },
+});
+
+exports.loadCSS = ({ include, exclude } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        include,
+        exclude,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+});
+
+exports.loadImages = ({ include, exclude, options } = {}) => ({
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        include,
+        exclude,
+        use: {
+          loader: 'url-loader',
+          options,
+        },
+      },
+    ],
+  },
+});
+
+exports.generateSourceMaps = ({ type }) => ({
+  devtool: type,
+});
+
+exports.setFreeVariable = (key, value) => {
+  const env = {};
+  env[key] = JSON.stringify(value);
+
+  return {
+    plugins: [new webpack.DefinePlugin(env)],
+  };
+};
